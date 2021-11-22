9225cf178dd20e0ca25adea302953296a035ae41
uzair-suria <uzair.a.suria@outlook.com>
2021-11-22 16:04:09 +0500

    fixed empty registration form submission bug

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,10 @@
 const handleRegistration = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
 
+  if (!email || !name || !password) {
+    return res.status(400).json("incorrect form submission");
+  }
+
   let salt = bcrypt.genSaltSync(10);
   let hash = bcrypt.hashSync(password, salt);
 
