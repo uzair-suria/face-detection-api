ade21fbc56ee97d8e7180bc07d120a07b156057e
uzair-suria <uzair.a.suria@outlook.com>
2021-11-23 22:46:12 +0500

    db boilerplate from dev docs

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const db = knex({
   client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: true,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 });
 
@@ -22,6 +24,20 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Home");
 });
+
+app.get("/db", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM test_table");
+    const results = { results: result ? result.rows : null };
+    res.render("pages/db", results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
 app.post("/signin", (req, res) => handleSigninRequest(req, res, db, bcrypt));
 app.post("/register", (req, res) => handleRegistration(req, res, db, bcrypt));
 app.get("/profile/:id", (req, res) => getProfile(req, res, db));
